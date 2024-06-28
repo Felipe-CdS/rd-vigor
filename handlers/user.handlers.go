942e8f6d0d26a5ef032bd93d518cc9e967558bc7
Felipe-CdS <felipe32santos@hotmail.com>
2024-06-28 11:06:58 -0300

    remove file needed

--- handlers/user.handlers.go
+++ handlers/user.handlers.go
@@ -1,15 +1,9 @@
 package handlers
 
 import (
-	"bytes"
-	"fmt"
-	"io"
 	"net/http"
 
 	"github.com/a-h/templ"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/labstack/echo/v4"
 	"nugu.dev/rd-vigor/services"
 	"nugu.dev/rd-vigor/views/login"
@@ -32,9 +26,6 @@ type UserHandler struct {
 func (uh *UserHandler) CreateNewUser(c echo.Context) error {
 
 	if c.Request().Method == "POST" {
-		// if c.FormValue("password") != c.FormValue("password") {
-		// 	return echo.NewHTTPError(http.StatusNotFound)
-		// }
 
 		user := services.User{
 			FirstName:      c.FormValue("first_name"),
@@ -46,46 +37,6 @@ func (uh *UserHandler) CreateNewUser(c echo.Context) error {
 		}
 
 		if err := uh.UserServices.CreateUser(user); err != nil {
-			return err
-		}
-
-		file, err := c.FormFile("occupation_file")
-
-		if err != nil {
-			return err
-		}
-
-		src, err := file.Open()
-
-		if err != nil {
-			return err
-		}
-
-		defer src.Close()
-
-		sess, err := session.NewSession(&aws.Config{
-			Region: aws.String("us-east-2"),
-		})
-
-		if err != nil {
-			return err
-		}
-
-		svc := s3.New(sess)
-
-		var buf bytes.Buffer
-
-		if _, err := io.Copy(&buf, src); err != nil {
-			return err
-		}
-
-		_, err = svc.PutObject(&s3.PutObjectInput{
-			Bucket: aws.String("rd-vigor-20050411"),
-			Key:    aws.String(fmt.Sprintf("%s_%s_%s", c.FormValue("first_name"), c.FormValue("last_name"), file.Filename)),
-			Body:   bytes.NewReader(buf.Bytes()),
-		})
-
-		if err != nil {
 			return err
 		}
 
